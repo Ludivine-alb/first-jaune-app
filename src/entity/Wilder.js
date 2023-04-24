8dd305eadaad018dc5f2162e0da9e9d676cff19e
Ludivine-alb <93869702+Ludivine-alb@users.noreply.github.com>
2023-04-24 16:22:51 +0200

    Creation of first entity Wilder

---
+++ src/entity/Wilder.js
@@ -0,0 +1,17 @@
+const { text } = require("stream/consumers");
+
+const EntitySchema = require("typeorm").EntitySchema;
+
+module.exports = new EntitySchema({
+    name: "Wilder",
+    columns: {
+        id:{
+            primary: true,
+            type: "int",
+            generated: true,
+        },
+        name: {
+            type: "text",
+        },
+    },
+});
